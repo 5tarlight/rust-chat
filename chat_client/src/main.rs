5d63df10fcb07cbde3c27e4f59cce84ba42efb70
YEAHx4 <developerstarlight@gmail.com>
2023-04-14 20:59:27 +0900

    test client

--- chat_client/src/main.rs
+++ chat_client/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use futures_channel::mpsc::UnboundedSender;
+use futures_util::{future, pin_mut, StreamExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+use url::Url;
+
+#[tokio::main]
+async fn main() {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| panic!("This program requires address in first argument"));
+    let url = Url::parse(&addr).unwrap();
+
+    let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
+    tokio::spawn(read_stdin(stdin_tx));
+
+    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect!");
+    println!("WebSocket handshake has been successfully completed");
+
+    let (write, read) = ws_stream.split();
+
+    let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+    let ws_to_stdout = {
+        read.for_each(|message| async {
+            let data = message.unwrap().into_data();
+            tokio::io::stdout().write_all(&data).await.unwrap();
+        })
+    };
+
+    pin_mut!(stdin_to_ws, ws_to_stdout);
+    future::select(stdin_to_ws, ws_to_stdout).await;
+}
+
+async fn read_stdin(tx: UnboundedSender<Message>) {
+    let mut stdin = tokio::io::stdin();
+
+    loop {
+        let mut buf = vec![0; 1024];
+
+        let n = match stdin.read(&mut buf).await {
+            Err(_) | Ok(0) => break,
+            Ok(n) => n,
+        };
+
+        buf.truncate(n);
+        tx.unbounded_send(Message::binary(buf)).unwrap();
+    }
 }
